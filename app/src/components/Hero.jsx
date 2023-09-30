1eb9e868a2a0400deccd5e2983a6e5b1007d8be5
Tharun Raj <tharun.2.linux@gmail.com>
2023-09-30 19:54:45 +0530

    changed the gradient color of hero

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="rounded-xl m-4 bg-gradient-to-br from-blue-300 to-blue-400">
+    <section className="rounded-xl m-4 bg-gradient-to-br from-blue-400 to-blue-500">
       <div className=" h-96 flex justify-center items-center ">
         <h1 className="text-3xl sm:text-6xl font-bold">Zip it. Freely.</h1>
       </div>
