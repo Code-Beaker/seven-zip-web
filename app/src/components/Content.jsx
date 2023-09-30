82646e10cfb6aa7b1fd57059c4547ea3ab719593
Tharun Raj <tharun.2.linux@gmail.com>
2023-09-30 18:56:19 +0530

    completed about 7-zip section

--- app/src/components/Content.jsx
+++ app/src/components/Content.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <main>
+      <About link={"about"} />
+    </main>
+  );
+};
+
+const About = ({ link }) => {
+  return (
+    <section className="px-12 sm:py-24 text-center" id={link}>
+      <h1 className="text-5xl my-3 font-bold text-slate-800">What is 7-zip?</h1>
+      <p className="text-2xl font-medium text-slate-600">
+        A <span className="font-bold">Free</span> and{" "}
+        <span className="font-bold">Open-source</span> archiver tool with high
+        compression rates.
+      </p>
+      <ViewSourceButton />
+    </section>
+  );
+};
+
+const ViewSourceButton = () => {
+  return (
+    <a
+      className="inline-flex text-lg my-4 font-medium gap-5 items-center px-5 py-3 border border-slate-200 rounded-full hover:bg-slate-100 transition-transform active:bg-slate-200 active:scale-95"
+      href=""
+    >
+      View source
+      <span>
+        <BsGithub />
+      </span>
+    </a>
+  );
 };
 
 export default Content;
