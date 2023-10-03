146af00dc429f85aa89bb3e1781283b6b8aca305
Tharun Raj <tharun.2.linux@gmail.com>
2023-10-03 17:41:07 +0530

    created footer file

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import TopBanner from "./components/TopBanner";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <Content />
+      <Footer />
     </>
   );
 };
