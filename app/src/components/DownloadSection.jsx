c40e4089bd2609a6788fff5e750a2cdca81b4d8a
Tharun Raj <tharun.2.linux@gmail.com>
2023-10-06 16:54:58 +0530

    loaded the os data from a json file.

--- app/src/components/DownloadSection.jsx
+++ app/src/components/DownloadSection.jsx
@@ -1,30 +1,34 @@
 import React from "react";
+import { windows, macos, linux } from "../data/osinfo.json";
 import { BsDownload } from "react-icons/bs";
 
 const DownloadSection = () => {
   return (
     <div className="grid grid-cols-3 gap-3 p-6">
       <DownloadItem
-        title={"Microsoft Windows"}
-        text={"Download Windows executable file."}
-        link={"#"}
+        title={windows.title}
+        text={windows.description}
+        format={windows.format}
+        link={windows.source}
       />{" "}
       <DownloadItem
-        title={"Apple MacOS"}
-        text={"Download MacOS disk image file."}
-        link={"#"}
+        title={macos.title}
+        text={macos.description}
+        format={macos.format}
+        link={macos.source}
       />{" "}
       <DownloadItem
-        title={"Linux"}
-        text={"Download debian installer file."}
-        link={"#"}
+        title={linux.title}
+        text={linux.description}
+        format={linux.format}
+        link={linux.source}
       />
     </div>
   );
 };
 
 const DownloadItem = (props) => {
-  const { title, text, link } = props;
+  const { title, text, link, format } = props;
 
   return (
     <div className="bg-slate-50 rounded p-6">
@@ -34,7 +38,7 @@ const DownloadItem = (props) => {
         className="bg-slate-200 inline-flex justify-center items-center px-5 py-2 rounded-full gap-2 hover:bg-slate-300 my-2 font-medium transition-transform active:bg-slate-400 active:scale-95"
         href={link}
       >
-        Download <BsDownload />
+        Download {format} <BsDownload />
       </a>
     </div>
   );
