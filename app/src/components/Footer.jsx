a7ea288bd12b425891cbf9ce2732f769e9e59ba8
Tharun Raj <tharun.2.linux@gmail.com>
2023-10-06 17:02:46 +0530

    added a footer to the site

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  let d = new Date();
+  const CurrentYear = d.getFullYear();
+
+  return (
+    <footer className="bg-slate-800 text-center py-24 px-12">
+      <p className="text-medium text-xl text-slate-100">
+        Copyright {CurrentYear}, All rights reserved.{" "}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
