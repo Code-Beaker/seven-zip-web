b3faa3ba1638e5fc58dc5fa6f15f9cffeb670c11
Tharun Raj <tharun.2.linux@gmail.com>
2023-10-08 08:02:04 +0530

    installed react-router-dom package

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,16 +6,18 @@ import Hero from "./components/Hero";
 import TopBanner from "./components/TopBanner";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import GoToTop from "./components/GoToTop";
 
 const App = () => {
   return (
-    <>
+    <main>
       <TopBanner />
+      <GoToTop />
       <NavBar />
       <Hero />
       <Content />
       <Footer />
-    </>
+    </main>
   );
 };
 
